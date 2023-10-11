dfed0666de4c3aa65b5a3b6cffc298e8476de602
Victor Lozano <lozano.victor.vhcl@gmail.com>
2023-10-11 11:05:49 -0300

    Atualizações da função de login e dashboard de gestor
    - Conectado com um banco de dados externo, porém ainda offline
    - Gráficos da dashbord ainda não funcionam por falta de conexão ao banco de dados.

--- app.R
+++ app.R
@@ -3,6 +3,9 @@ library(shinydashboard)
 library(DT)
 library(shinyjs)
 library(sodium)
+library(billboarder)
+library(RSQLite)
+library(DBI)
 
 # Main login screen
 loginpage <- div(id = "loginpage", style = "width: 500px; max-width: 100%; margin: 0 auto; padding: 20px;",
@@ -24,25 +27,24 @@ loginpage <- div(id = "loginpage", style = "width: 500px; max-width: 100%; margi
                                   class = "text-center"))),
                      br(),
                      br(),
-                     tags$code("Username: admin, prof, gest, equi"),
+                     tags$code("Username: admin, prof, gest, equi, gestor"),
                      br(),
-                     tags$code("Password: admin, prof, gest, equi"), 
+                     tags$code("Password: admin, prof, gest, equi, gestor"), 
                      br(),
                      tags$code("match them")
                    ))
 )
 
-credentials = data.frame(
-  #NOMES DE USUÁRIO (LINKAR COM O RSQLITE)
-  username_id = c("admin", "prof", "gest", "equi"),
-  
-  #SENHAS (LINKAR COM O RSQLITE)
-  passod = sapply(c("admin", "prof", "gest", "equi"),password_store),
-  
-  #SENHAS (CRIAR UM CAMPO EXTRA PRA LINKAR NO SQL)
-  permission = c("advanced", "prof", "gest", "equi"), 
-  stringsAsFactors = F
-)
+#Cria uma conexão com o banco de dados
+#Essa conexão vai ser usada para todas as consultas no banco de dados
+#Alterar o caminho para o servidor
+dados_bd <- dbConnect(RSQLite::SQLite(), "D:/app-1/app/teste-funcoes/teste.db")
+#Seleciona os dados de login da tabela usuario
+credentials <- dbGetQuery(dados_bd, "SELECT telefone AS username_id, senha AS passod, email, `tipo-usuario` AS permission FROM usuario", stringsAsFactors = F)
+#Aplica um hash na coluna das senhas
+credentials$passod <- sapply(credentials$passod, password_store)
+
+
 
 header <- dashboardHeader( title = "Painel Lateral", uiOutput("logoutbtn"))
 
@@ -81,6 +83,7 @@ server <- function(input, output, session) {
   
   output$logoutbtn <- renderUI({
     req(USER$login)
+    dbDisconnect(dados_login)
     tags$li(a(icon("fa fa-sign-out"), "Logout", 
               href="javascript:window.location.reload(true)"),
             class = "dropdown", 
@@ -121,11 +124,47 @@ server <- function(input, output, session) {
                 menuItem("About Page", tabName = "About", icon = icon("th"))
               )
             }
+            else{
+              if (credentials[,"permission"][which(credentials$username_id==input$userName)]=="gestor") {
+                sidebarMenu(
+                  menuItem("Gestante", tabName = "Gestante", icon = icon("person-pregnant")),
+                  menuItem("Profissional", tabName = "Profissional", icon =icon("user-doctor"))
+                )
+              }
+            }
           }
         }
       }
     }
   })
+  
+  #dados
+  #Adicionar externamente para não quebrar o Shiny
+  data("gestantes")
+  gestantes_idade <- dbGetQuery(dados_bd, "SELECT `respostas-questionario-inicial-triagem-gestante`.`a9-idade-em-anos-completos` AS `a9-idade-em-anos-completos`, count(*) AS `count`
+                                           FROM `respostas-questionario-inicial-triagem-gestante`
+                                           WHERE ((`respostas-questionario-inicial-triagem-gestante`.`a9-idade-em-anos-completos` <> 15
+                                           OR `respostas-questionario-inicial-triagem-gestante`.`a9-idade-em-anos-completos` IS NULL)
+                                           AND (`respostas-questionario-inicial-triagem-gestante`.`a9-idade-em-anos-completos` <> 16 OR `respostas-questionario-inicial-triagem-gestante`.`a9-idade-em-anos-completos` IS NULL) AND (`respostas-questionario-inicial-triagem-gestante`.`a9-idade-em-anos-completos` <> 17 OR `respostas-questionario-inicial-triagem-gestante`.`a9-idade-em-anos-completos` IS NULL) AND `respostas-questionario-inicial-triagem-gestante`.`a14a-tem-restricao-medica` = 'Não' AND `respostas-questionario-inicial-triagem-gestante`.`a17-concordou-em-participar-e-assinou-tcle` = 'Sim')
+                                           GROUP BY `respostas-questionario-inicial-triagem-gestante`.`a9-idade-em-anos-completos`
+                                          ORDER BY `respostas-questionario-inicial-triagem-gestante`.`a9-idade-em-anos-completos` ASC")
+  
+  gestantes_semanas <-dbGetQuery(dados_bd, "SELECT `respostas-questionario-inicial-triagem-gestante`.`a10-semana-gestacional` AS `a10-semana-gestacional`, count(*) AS `count`
+                                            FROM `respostas-questionario-inicial-triagem-gestante`
+                                            GROUP BY `respostas-questionario-inicial-triagem-gestante`.`a10-semana-gestacional`
+                                            ORDER BY `respostas-questionario-inicial-triagem-gestante`.`a10-semana-gestacional` ASC")
+  
+  gestantes_consultas_prenatal <- dbGetQuery(dados_bd, "SELECT `respostas-questionario-inicial-triagem-gestante`.`a11-consulta-pre-natal-numero` AS `a11-consulta-pre-natal-numero`, count(*) AS `count`
+                                                        FROM `respostas-questionario-inicial-triagem-gestante`
+                                                        GROUP BY `respostas-questionario-inicial-triagem-gestante`.`a11-consulta-pre-natal-numero`
+                                                        ORDER BY `respostas-questionario-inicial-triagem-gestante`.`a11-consulta-pre-natal-numero` ASC")
+  
+  gestantes_entrevistadora <-dbGetQuery(dados_bd, "SELECT `respostas-questionario-inicial-triagem-gestante`.`a1-entrevistador` AS `a1-entrevistador`, count(*) AS `count`
+                                                   FROM `respostas-questionario-inicial-triagem-gestante`
+                                                   GROUP BY `respostas-questionario-inicial-triagem-gestante`.`a1-entrevistador`
+                                                   ORDER BY `count` DESC, `respostas-questionario-inicial-triagem-gestante`.`a1-entrevistador` ASC")
+  #fim dos dados
+  
   
   #Renderiza as abas permitidas do usuário
   output$body <- renderUI({
@@ -196,7 +235,57 @@ server <- function(input, output, session) {
               )
             }
             else{
+              #Credencial de gestor
+              if (credentials[,"permission"][which(credentials$username_id==input$userName)]=="gestor") {
+                tabItems(
+                  
+                  tabItem(
+                    tabName ="Gestante", class ="active",
+                    fluidRow(
+                      # pie chart Gestantes por idade
+                      billboarder() %>% 
+                        bb_piechart(data = gestantes_idade) %>% 
+                        bb_labs(title = "Gestantes por idade",
+                                caption = "")
+                      ),
+                    fluidRow(
+                      # pie chart Gestantes por semana de gestação
+                      billboarder() %>% 
+                        bb_piechart(data = gestantes_semanas) %>% 
+                        bb_labs(title = "Gestantes por semanas de gestação",
+                                caption = "")
+                    ),
+                    fluidRow(
+                      # pie chart Gestantes por número de consultas pré natal
+                      billboarder() %>% 
+                        bb_piechart(data = gestantes_consultas_prenatal) %>% 
+                        bb_labs(title = "Gestantes por número de consultas pré natal",
+                                caption = "")
+                    ),
+                    fluidRow(
+                      # pie chart Percentual de gestantes por entrevistadora
+                      billboarder() %>% 
+                        bb_piechart(data = gestantes_entrevistadora) %>% 
+                        bb_labs(title = "Percentual de gestantes por entrevistadora",
+                                caption = "")
+                    )
+                    ),
+                  
+                    tabItem(
+                      tabName = "Profissional",
+                      fluidRow(
+                        billboarder() %>% 
+                          bb_piechart(data = gestantes) %>% 
+                          bb_labs(title ="Dados de profissional",
+                                  caption ="Data source: RTE (https://opendata.rte-france.com)")
+                        )
+                      )
+                    )
+
+              }
+              else{
               loginpage
+              }
             }
           }
         }
@@ -221,4 +310,4 @@ server <- function(input, output, session) {
   
 }
 
-runApp(list(ui = ui, server = server), launch.browser = TRUE)
+runApp(list(ui = ui, server = server))
